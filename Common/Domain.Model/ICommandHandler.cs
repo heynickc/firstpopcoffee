569b3a66a6876ef770bc6bfc5c2f57307340d3d4
Nick Chamberlain <nick.chamberlain.jr@gmail.com>
2016-10-24 14:56:48 -0400

    command handlers

---
+++ Common/Domain.Model/ICommandHandler.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Common.Domain.Model {
+    public interface ICommandHandler<TCommand> where TCommand : Message {
+        void Handle(TCommand message);
+    }
+}
---
+++ RoastPlanning/Domain.Model/CreateRoastScheduleCommandHandler.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Common.Domain.Model;
+
+namespace RoastPlanning.Domain.Model {
+    public class CreateRoastScheduleCommandHandler : ICommandHandler<CreateRoastSchedule> {
+        private readonly IRepository<RoastSchedule> _repository;
+
+        public CreateRoastScheduleCommandHandler(IRepository<RoastSchedule> repository) {
+            _repository = repository;
+        }
+
+        public void Handle(CreateRoastSchedule message) {
+            var roastSchedule = new RoastSchedule(Guid.NewGuid());
+            _repository.Save(roastSchedule);
+        }
+    }
+}
